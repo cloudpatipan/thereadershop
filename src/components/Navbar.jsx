021b0cd15eb19d0137ef25d9003978321adc9971
cloudpatipan <117970258+cloudpatipan@users.noreply.github.com>
2024-06-26 06:12:56 +0700

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -304,26 +304,19 @@ export default function Navbar() {
             <Link to={'/category'} className="bg-white flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1">
               ประเภทสินค้า
             </Link>
-            <button className="bg-white flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1" onClick={openModalLogin}>
-              เข้าสู่ระบบ
-            </button>
+            {!localStorage.getItem('token') ? (
+              <div>
+                <button className="bg-white flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1" onClick={openModalLogin}>
+                  เข้าสู่ระบบ
+                </button>
 
-            <Modal isOpen={isModalOpenLogin} onClose={closeModalLogin}>
-              <Login />
-            </Modal>
+                <button className="bg-white flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1" onClick={openModalRegister}>
+                  สมัครสมาชิก
+                </button>
+              </div>
+            ) : null}
 
-            <button className="bg-white flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1" onClick={openModalRegister}>
-              สมัครสมาชิก
-            </button>
 
-            <Modal isOpen={isModalOpenRegister} onClose={closeModalRegister}>
-              <Register />
-            </Modal>
-            {user && user.role == 'admin' && ( // เช็คว่ามีผู้ใช้ล็อคอินและเป็น admin หรือไม่
-              <Link to={'/admin/product'} className="bg-white relative flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1">
-                แอดมิน
-              </Link>
-            )}
           </div>
         ) : null}
 
