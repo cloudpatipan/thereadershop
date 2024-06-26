dcd18ce5650f6a7a65b5ab0b3178946e13a2d5e9
cloudpatipan <117970258+cloudpatipan@users.noreply.github.com>
2024-06-26 17:16:09 +0700

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -217,9 +217,9 @@ export default function Navbar() {
                     header={
                       <div className="w-[3rem] h-[3rem] rounded-full overflow-hidden cursor-pointer" onClick={handleToggleDropdown}>
                         {user.avatar ? (
-                          <img className="w-full h-full object-cover" src={`https://c45c-2405-9800-b540-dc40-d538-9787-5ce1-fc75.ngrok-free.app/images/avatar/${user.avatar}`} alt={`รูปภาพของ ${user.name}`} />
+                          <img className="w-full h-full object-cover" src={`https://ef9c-2405-9800-b540-dc40-a46a-cab9-89b-365c.ngrok-free.app/images/avatar/${user.avatar}`} alt={`รูปภาพของ ${user.name}`} />
                         ) : (
-                          <img className="w-full h-full object-cover" src="https://c45c-2405-9800-b540-dc40-d538-9787-5ce1-fc75.ngrok-free.app/images/product/No_image.png" alt="No Image" />
+                          <img className="w-full h-full object-cover" src="https://ef9c-2405-9800-b540-dc40-a46a-cab9-89b-365c.ngrok-free.app/images/product/No_image.png" alt="No Image" />
                         )}
                       </div>
                     }
