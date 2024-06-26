c369e01d2d744d584b8a762112cd9250e464d7b6
cloudpatipan <117970258+cloudpatipan@users.noreply.github.com>
2024-06-26 12:49:26 +0700

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -304,7 +304,7 @@ export default function Navbar() {
             <Link to={'/category'} className="bg-white flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1">
               ประเภทสินค้า
             </Link>
-                      {!localStorage.getItem('token') ? (
+            {!localStorage.getItem('token') ? (
               <div>
                 <button className="w-full bg-white flex justify-center items-center border rounded-full bg-transparent py-2 px-5 font-medium uppercase text-black hover:text-white hover:bg-black transition-all duration-300 mt-1" onClick={openModalLogin}>
                   เข้าสู่ระบบ
