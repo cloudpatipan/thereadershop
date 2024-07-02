74326c5b89abdae41a2789da98db103c3d933881
cloudpatipan <117970258+cloudpatipan@users.noreply.github.com>
2024-07-02 12:59:31 +0700

    Update BaseUrl.jsx

--- src/routes/BaseUrl.jsx
+++ src/routes/BaseUrl.jsx
@@ -1,3 +1,3 @@
-const baseUrl = 'https://2c89-2405-9800-b540-dc40-170-1ea4-fb6d-cdbb.ngrok-free.app';
+const baseUrl = 'https://b224-2405-9800-b540-dc40-1982-2ad5-36fc-fe6f.ngrok-free.app';
 
 export default baseUrl;
